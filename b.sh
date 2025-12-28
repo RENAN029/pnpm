aed2f4dbc77ca30c9625ed0c1207692df51bc740
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-28 09:14:53 -0300

    Update b.sh

--- b.sh
+++ b.sh
@@ -16,7 +16,7 @@ cd ..
 rm -rf shader-patcherx
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-echo "sudo pacman -S fzf starship fish fisher ripgrep zoxide mcfly tldr eza bat fd lazygit lazydocker" 
+echo "sudo pacman -S fzf starship fish fisher ripgrep zoxide eza bat fd lazygit github-cli lazydocker" 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 sudo pacman -Scc --noconfirm
