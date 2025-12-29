a21b65fb7c0da5cbb970edd5bc8da325b2f2f8c1
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-29 10:28:32 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -25,6 +25,6 @@ echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee
 
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
-echo "sudo pacman -S zerotier-one httpie iwd wpa_supplicant xdg-utils github-cli croc libqalculate superfile zellij xh"
+echo "sudo pacman -S"
 cd ..
 rm -rf jq 
