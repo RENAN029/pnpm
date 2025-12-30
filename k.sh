300a525111718807a94c9028ca733051a7b41be4
RENAN029 <pires.s@escolar.ifrn.edu.br>
2025-12-30 10:19:12 -0300

    Update k.sh

--- k.sh
+++ k.sh
@@ -3,7 +3,7 @@ set -e
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
 sudo pacman -S --noconfirm ark nvidia-open intel-ucode git neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 earlyoom  
-sudo pacman -S --noconfirm plasma-meta kdeconnect konsole dolphin partitionmanager ffmpegthumbs dolphin-plugins sweeper 
+sudo pacman -S --noconfirm plasma-meta konsole dolphin discover kdeconnect partitionmanager ffmpegthumbs dolphin-plugins 
 echo "sudo pacman -S"
 sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
 git clone https://github.com/maximilionus/lucidglyph.git
